5924038589f1935ea9da09d3b5aac422f8cf9fec
yc840 <54966502+yc840@users.noreply.github.com>
2019-12-04 22:46:51 -0500

    Dray Creation and update on the scripts

--- Assets/__Scripts/TileCamera.cs
+++ Assets/__Scripts/TileCamera.cs
@@ -38,12 +38,13 @@ public class TileCamera : MonoBehaviour
 
         System.Globalization.NumberStyles hexNum;
         hexNum = System.Globalization.NumberStyles.HexNumber;
+        
         //Place the map data into a 2D Array for faster access
         MAP = new int[W, H];
-        for(int j=0; j<H; j++)
+        for(int j = 0; j < H; j++)
         {
             tileNums = lines[j].Split(' ');
-            for(int i = 0; i<W; i++)
+            for(int i = 0; i < W; i++)
             {
                 if(tileNums[i] == "..")
                 {
@@ -51,20 +52,56 @@ public class TileCamera : MonoBehaviour
                 }
                 else
                 {
-                    MAP[i, j] = int.Parse(tileNums[i], hexNum);
+
+                    MAP[i, j] = int.Parse(tileNums[i], hexNum);  
+                    
                 }
             }
         }
-        print("Parsed" + SPRITES.Length + "sprites.");
+        print("Parsed " + SPRITES.Length + "sprites.");
         print("Map size: " + W + "wide by" + H + "high");
 
+        ShowMap();
+    } 
+    void ShowMap()
+    {
+        TILES = new Tile[W, H];
+        for(int j = 0; j < H; j++)
+        {
+            for(int i=0; i<W; i++)
+            {
+                if (MAP[i, j] != 0)
+                {
+                    Tile ti = Instantiate<Tile>(tilePrefab);
+                    ti.transform.SetParent(TILE_ANCHOR);
+                    ti.SetTile(i, j);
+                    TILES[i, j] = ti;
+                }
+            }
+        }
     }
+       
     static public int GET_MAP(int x, int y)
     {
-        if(x<0 || y<0 || y >= H)
+        if(x < 0 || x >= W || y < 0 || y >= H)
         {
             return -1;
         }
         return MAP[x, y];
+    } 
+    static public int GET_MAP(float x, float y)
+    {
+        int tX = Mathf.RoundToInt(x);
+        int tY = Mathf.RoundToInt(y - 0.25f);
+        return GET_MAP(tX, tY);
+
+    } 
+    static public void SET_MAP( int x, int y, int tNum)
+    {
+        if(x < 0 || x >= W || y < 0 || y >=  H)
+        {
+            return;
+        }
+        MAP[x, y] = tNum;
     }
 }
